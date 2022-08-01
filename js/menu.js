1f323e7fe465d5b66de53ce5ab50b572922c69bf
soolomiia <106016596+soolomiia@users.noreply.github.com>
2022-08-01 14:39:31 +0300

    модалка

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,14 @@
-
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })();
+  const refs = {
+    openMobileMenuBtn: document.querySelector("[data-mobile-menu-open]"),
+    closeMobileMenuBtn: document.querySelector("[data-mobile-menu-close]"),
+    mobileMenu: document.querySelector("[data-mobile-menu]"),
+  };
+
+  refs.openMobileMenuBtn.addEventListener("click", toggleMobileMenu);
+  refs.closeMobileMenuBtn.addEventListener("click", toggleMobileMenu);
+
+  function toggleMobileMenu() {
+    refs.mobileMenu.classList.toggle("is-hidden");
+  }
+})();
